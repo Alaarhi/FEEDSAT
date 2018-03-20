88b2aff5396102f02f6e9313ffe56d215b675013
unknown <hamza.gaaliche@gmail.com>
2018-03-20 14:19:24 +0100

    comit

--- loginForm.php
+++ loginForm.php
@@ -1,5 +1,110 @@
 
-<div id="modal-wrapper" class="modal" >
+<div id="avis" class="modal" >
+  <form class="modal-content animate" action="avis.php" method="post">
+    
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="fermer">&times;</span>
+        <img src="img/medical-link.jpg" alt="Logo">
+          <h4 style="margin-top: 5%; text-align:center; color: #5e6467">Identifiez-vous pour accéder à ce contenu</h4>
+    </div>
+      
+    <div class="container">
+      <input name="numInscri" class="pm-form-textfield" type="text" placeholder="Identifiant (votre numéro d'étudiant)">
+      <input name="numCin" class="pm-form-textfield" type="password" placeholder="Mot de Passe (votre numéro de cin)">        
+      <button style= 
+      "
+        background-color:  #303F9F;
+        color: white;
+        padding: 14px 20px;
+        margin: 15px 30px;
+        border: none;
+        cursor: pointer;
+        width: 50%;
+        font-size:15px;
+        font-family: 'Raleway';
+        transition: all 0.5s;
+        -moz-transition: all 0.5s;
+        -webkit-transition: all 0.5s;
+        -o-transition: all 0.5s;
+      "   
+      type="submit" >SE CONNECTER</button>  
+    </div>
+  </form>
+</div>
+
+
+
+<div id="commentaire" class="modal" >
+  <form class="modal-content animate" action="avis.php" method="post">
+    
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="fermer">&times;</span>
+        <img src="img/medical-link.jpg" alt="Logo">
+          <h4 style="margin-top: 5%; text-align:center; color: #5e6467">Identifiez-vous pour accéder à ce contenu</h4>
+    </div>
+      
+    <div class="container">
+      <input name="numInscri" class="pm-form-textfield" type="text" placeholder="Identifiant (votre numéro d'étudiant)">
+      <input name="numCin" class="pm-form-textfield" type="password" placeholder="Mot de Passe (votre numéro de cin)">        
+      <button style= 
+      "
+        background-color:  #303F9F;
+        color: white;
+        padding: 14px 20px;
+        margin: 15px 30px;
+        border: none;
+        cursor: pointer;
+        width: 50%;
+        font-size:15px;
+        font-family: 'Raleway';
+        transition: all 0.5s;
+        -moz-transition: all 0.5s;
+        -webkit-transition: all 0.5s;
+        -o-transition: all 0.5s;
+      "   
+      type="submit" >SE CONNECTER</button>  
+    </div>
+  </form>
+</div>
+
+
+
+
+<div id="vote" class="modal" >
+  <form class="modal-content animate" action="avis.php" method="post">
+    
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="fermer">&times;</span>
+        <img src="img/medical-link.jpg" alt="Logo">
+          <h4 style="margin-top: 5%; text-align:center; color: #5e6467">Identifiez-vous pour accéder à ce contenu</h4>
+    </div>
+      
+    <div class="container">
+      <input name="numInscri" class="pm-form-textfield" type="text" placeholder="Identifiant (votre numéro d'étudiant)">
+      <input name="numCin" class="pm-form-textfield" type="password" placeholder="Mot de Passe (votre numéro de cin)">        
+      <button style= 
+      "
+        background-color:  #303F9F;
+        color: white;
+        padding: 14px 20px;
+        margin: 15px 30px;
+        border: none;
+        cursor: pointer;
+        width: 50%;
+        font-size:15px;
+        font-family: 'Raleway';
+        transition: all 0.5s;
+        -moz-transition: all 0.5s;
+        -webkit-transition: all 0.5s;
+        -o-transition: all 0.5s;
+      "   
+      type="submit" >SE CONNECTER</button>  
+    </div>
+  </form>
+</div>
+
+
+<div id="applaud" class="modal" >
   <form class="modal-content animate" action="avis.php" method="post">
     
     <div class="imgcontainer">
