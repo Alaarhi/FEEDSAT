e86d551a4e3ce6a033883a9e5c799b9987561ed1
Alaarhi <alaarhi@github.com>
2018-04-07 13:16:18 +0100

    backend

---
+++ approuver.php
@@ -0,0 +1,12 @@
+<?php
+include '../dbConnection.php';
+
+$var = $_POST['id'];
+
+if ($req = $bd->query("UPDATE comment SET approved = 1 WHERE id=".$var)) {
+    echo "comment approved";
+}
+
+else {echo "error ";}
+
+?>
