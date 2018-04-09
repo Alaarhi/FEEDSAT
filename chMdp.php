d68870378bf4acd6eaf655b2826898ed5db7a4c6
unknown <hamza.gaaliche@gmail.com>
2018-04-09 04:53:38 +0100

    commit

--- chMdp.php
+++ chMdp.php
@@ -1,6 +1,6 @@
 <?php 
     include 'dbConnection.php';
-    $req=$bd->query("update student set password=".$_POST['mdp']." where id=".$_POST['studentId']);
+    $req=$bd->query("update student set password='".$_POST['mdp']."' where id=".$_POST['studentId']);
     if($req)
     {
     echo "Mot de passe changé avec succès.";
